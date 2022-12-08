03a83b975d77cb1d2757909da5668f91ff6fff40
alex-bada <alexbadaoui99@gmail.com>
2022-12-08 14:34:39 +0100

    Class aeroport + test

--- src/aeroport.java
+++ src/aeroport.java
@@ -1,9 +1,17 @@
 public class aeroport {
-    private String IATA;    //attribut
-    private String Name;
-    private String country;
+    private final String IATA;    //attribut
+    private final String Name;
+    private final String country;
     double latitude = 0;    // verify impossible coordinate
     double longitude = 0;
+
+    public aeroport(String iata, String name, String country, double latitude, double longitude) { //constructeur
+        this.IATA = iata;
+        this.Name = name;
+        this.country = country;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 
     public double getLatitude() {       //methode
         return latitude;
@@ -19,6 +27,14 @@ public class aeroport {
 
     @Override
     public String toString() {
-        return super.toString();
+        //return super.toString();
+        return "Aeroport " + Name+ " IATA " + IATA + " Lat " + latitude
+                + " Long " + longitude;
     }
+
+    public static void main(String args[]) {
+        aeroport a1= new aeroport("CGY", "Cergy", "France", 50, 50.5);
+        System.out.println(a1.toString());
+    }
+
 }
